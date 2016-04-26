ac94a665c563fe291b18744b29bbf2c1fffd6e89
chuiyuan <lisjthu@sina.com>
2016-04-26 17:38:12 +0800

    dev init

--- app/src/main/java/com/woaigsc/gscapp/MainActivity.java
+++ app/src/main/java/com/woaigsc/gscapp/MainActivity.java
@@ -1,13 +1,9 @@
 package com.woaigsc.gscapp;
 
 import android.app.Activity;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.content.res.Configuration;
 import android.os.Bundle;
 
-import com.woaigsc.gscapp.test.LMFragment;
-import com.woaigsc.gscapp.test.PMFragment;
+
 
 public class MainActivity extends Activity {
 
@@ -16,19 +12,5 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Configuration config = getResources().getConfiguration();
-        FragmentManager fragmentManager = getFragmentManager() ;
-        FragmentTransaction fragmentTransaction =
-                fragmentManager.beginTransaction() ;
-
-        if(config.orientation == Configuration.ORIENTATION_LANDSCAPE){
-            LMFragment lmFragment = new LMFragment();
-            fragmentTransaction.replace(android.R.id.content, lmFragment);
-        }else{
-            PMFragment pmFragment = new PMFragment();
-            fragmentTransaction.replace(android.R.id.content, pmFragment);
-        }
-        fragmentTransaction.commit() ;
-        new PMFragment();
     }
 }

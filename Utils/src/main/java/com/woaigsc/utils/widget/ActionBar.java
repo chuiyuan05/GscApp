a6f6a62a9316e17308d172d8a7cde8046bd4c673
chuiyuan <lisjthu@sina.com>
2016-05-07 20:31:44 +0800

    actionbar2

--- Utils/src/main/java/com/woaigsc/utils/widget/ActionBar.java
+++ Utils/src/main/java/com/woaigsc/utils/widget/ActionBar.java
@@ -1,6 +1,7 @@
 package com.woaigsc.utils.widget;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,8 @@ import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.woaigsc.utils.R;
 
 /**
  * Created by chuiyuan on 16-5-6.
@@ -28,5 +31,28 @@ public class ActionBar extends RelativeLayout {
     public ActionBar(Context context, AttributeSet attrs){
         super(context, attrs);
         mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        mBarView = (RelativeLayout)mInflater.inflate(R.layout.actionbar, null);
+        this.addView(mBarView);
+        mLogoView = (ImageView)mBarView.findViewById(R.id.actionbar_home_logo);
+        mHomeLayout = (RelativeLayout)mBarView.findViewById(R.id.actionbar_home_bg);
+        mHomeButton = (ImageButton)mBarView.findViewById(R.id.actionbar_home_btn);
+        mBackIndicator = mBarView.findViewById(R.id.actionbar_home_is_back);
+
+        mTitleView = (TextView)mBarView.findViewById(R.id.actionbar_title);
+        mActionsView = (LinearLayout)mBarView.findViewById(R.id.actionbar_actions);
+
+        mProgress = (ProgressBar)mBarView.findViewById(R.id.actionbar_progress);
+
+        TypedArray a = context.obtainStyledAttributes(attrs,R.styleable.ActionBar);
+        CharSequence title = a.getString(R.);
+
     }
 }
+
+
+
+
+
+
+
